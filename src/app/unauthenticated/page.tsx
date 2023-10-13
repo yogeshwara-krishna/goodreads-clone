953db69ac13f5037adf46f012aa30df1137cd000
K Yogeshwara Krishna <yogeshwara.k@dashtoon.com>
2023-10-13 14:29:36 +0530

    bug

--- src/app/unauthenticated/page.tsx
+++ src/app/unauthenticated/page.tsx
@@ -2,7 +2,6 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { Database } from "../../../types/database.types";
-import BubbleWrap from "../login/BubbleWrap";
 
 export default async function Unauthenticated() {
   const supabase = createServerComponentClient<Database>({ cookies });
