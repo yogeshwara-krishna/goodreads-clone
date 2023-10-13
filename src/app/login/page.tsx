6fcadeab4e9eb7a484bc04bc554bfa375cc4350b
K Yogeshwara Krishna <yogeshwara.k@dashtoon.com>
2023-10-13 14:18:47 +0530

    -

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,43 @@
-//Write a  hellow world component in react
-import React from 'react';
+"use client";
 
-export default function Login ()  {
-    return <div>Login</div>
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { Database } from "../../../types/database.types";
+import BubbleWrap from "./BubbleWrap";
+
+export default function Login() {
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email: "jon@supabase.com",
+      password: "sup3rs3cur3",
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+    router.refresh();
+  };
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email: "jon@supabase.com",
+      password: "sup3rs3cur3",
+    });
+    router.refresh();
+  };
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <div className="flex gap-2">
+      <button onClick={handleSignUp}>Sign up</button>
+      <button onClick={handleSignIn}>Sign in</button>
+      <button onClick={handleSignOut}>Sign out</button>
+    </div>
+  );
 }
